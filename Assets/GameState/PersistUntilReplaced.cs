956aae7a73593d35390338eec5e7ee148941da55
Jeff Sallans <jeffsallans@gmail.com>
2016-02-06 13:32:51 -0500

    BG Music: added background music

---
+++ Assets/GameState/PersistUntilReplaced.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PersistUntilReplaced : MonoBehaviour {
+
+    /// <summary>
+    /// Name of the object this is on
+    /// </summary>
+    public string OBJECT_NAME = "";
+
+    // Use this for initialization
+    void Awake()
+    {
+        //OBJECT_NAME must be defined
+        if (OBJECT_NAME == "")
+        {
+            throw new UnityException("PersistUntilReplace requires object name not be null");
+        }
+
+        //Current game object name must not match
+        if (gameObject.name == OBJECT_NAME)
+        {
+            throw new UnityException("PersistUntilReplace requires the game object be named different than OBJECT_NAME");
+        }
+
+        //Keep state over multiple screens
+        DontDestroyOnLoad(gameObject);
+
+        //If another Input Manager is detected, remove it
+        var oldObject = GameObject.Find(OBJECT_NAME);
+
+        if (oldObject != null)
+        {
+            Destroy(oldObject);
+        }
+
+        //Assign name
+        gameObject.name = OBJECT_NAME;
+    }
+
+    /// <summary>
+    /// Returns the current object
+    /// </summary>
+    /// <returns></returns>
+    public static GameObject getCurrent(string objectName)
+    {
+        //Current object, before the replace happends
+        var gameObjectBeforeInitFinishes = GameObject.Find("NEW_" + objectName);
+        if (gameObjectBeforeInitFinishes != null)
+        {
+            return gameObjectBeforeInitFinishes;
+        }
+
+        //Current object, after the replace happends
+        return GameObject.Find(objectName);
+    }
+}
---
+++ README.md
@@ -0,0 +1,34 @@
+## Summary
+
+A simple fighting game made over a weekend.  It strives to be the fastest fighting game!
+
+## Setup
+
+Get Unity 5.0 or greater (I am using 5.2.3f1)
+
+Add Unity package XboxCtrlrInput https://github.com/JISyed/Unity-XboxCtrlrInput#installation
+
+Initial scene Assets/Scenes/start.unity
+
+## Common Unity Scripts
+
+* Persist Until Replace
+* Input Manager
+* 2D Multiplayer Template
+
+## Contributors
+ 
+[Jeff Sallans](http://github.com/JeffSallans)
+
+## Credits
+
+[Deadlock By Will Bedford](https://soundcloud.com/2-byte/epic-tv-intro)
+[astrosound By AudioJungle](https://soundcloud.com/astrosoundmusic/astrosound-upbeat-electronic)
+
+## License
+
+[GNU GENERAL PUBLIC LICENSE](https://tldrlegal.com/license/gnu-general-public-license-v2)
+
+Version 2, June 1991
+
+
